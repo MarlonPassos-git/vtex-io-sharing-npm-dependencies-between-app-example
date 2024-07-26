5436b4be39e62dc7c80e9054649a17fa10d6e20d
marlonpassos <marlon.passos@cadastra.com>
2024-07-26 17:03:18 -0300

    setup

---
+++ app2/react/ComponentApp2.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Swiper, SwiperSlide } from "swiper/react"
+import "swiper/swiper.min.css"
+
+export default function ComponentApp2() {
+  return <div>
+      <div>ComponentApp2</div>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        style={{height: 100, width: "100%"}}
+        >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+      </Swiper>
+      </div>
+}
